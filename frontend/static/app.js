5a4438c5b5280b374828d3077f0f195face3dda7
gsc <gabrielschincariol@gmail.com>
2021-07-03 15:32:50 -0300

    Added: total jokes feature at footer

--- frontend/static/app.js
+++ frontend/static/app.js
@@ -1,6 +1,7 @@
 
 const jokeButton = document.querySelector('#jokeButton');
 const jokeDisplay = document.querySelector('.joke');
+const totalJokes = document.querySelector('.totalJokes');
 
 
 const getJoke = async () => {
@@ -10,9 +11,20 @@ const getJoke = async () => {
     return randomJoke.line
 }
 
+const getTotalJokes = async () => {
+    const response = await axios.get('/api');
+    const numberOfJokes = await response.data.length;
+    return numberOfJokes
+}
+
+const displayTotalJokes = async () => {
+    const numberOfJokes = await getTotalJokes();
+    totalJokes.textContent = `Total de piadas registradas: ${await numberOfJokes}`;
+}
+
 const jokeHandler = async () => {
     jokeDisplay.textContent = await getJoke()
 }
 
 jokeButton.addEventListener('click', jokeHandler)
-
+window.addEventListener('DOMContentLoaded', displayTotalJokes)
